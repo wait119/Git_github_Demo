4175bf3e19d3e0733ae235aca76a324625af6fe0
小斌 <1196836266@qq.com>
2021-05-29 09:45:57 +0800

    implement: buttonAdd onClick listener
    完成了加一的功能

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = "0"
+        add.setOnClickListener {
+            number++
+            textView.text = "$number"
+        }
     }
 }
